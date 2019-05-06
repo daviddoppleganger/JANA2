0df00d9b7c143d04e55c4e96837cdcfc24e7ab1c
David Lawrence <davidl@jlab.org>
2019-05-06 07:56:08 -0400

    Add Finish method. Set number of events to 100 and number of threads to 4.

--- src/plugins/janapy/MyProcessor.py
+++ src/plugins/janapy/MyProcessor.py
@@ -9,11 +9,14 @@ class MyProcessor( janapy.JEventProcessor):
 		super().__init__(self)
 
 	def Init( self ):
-		print('Python Init')
+		print('Python Init called')
 
 	# event is a JEvent object defined in janapy module
 	def Process( self ):
-		print('Python Process')
+		print('Python Process called')
+
+	def Finish( self ):
+		print('Python Finish called')
 
 #		hits = event.Get( DHit )
 #		for h in hits:
@@ -22,11 +25,14 @@ class MyProcessor( janapy.JEventProcessor):
 #-----------------------------------------------------------
 
 janapy.SetParameterValue( 'JANA:DEBUG_PLUGIN_LOADING', '1')
-janapy.SetParameterValue( 'NTHREADS', '1')
+janapy.SetParameterValue( 'NTHREADS', '4')
+janapy.SetParameterValue( 'NEVENTS', '100')
 
 # The janapy module itself serves as the JApplication facade
 janapy.AddProcessor( MyProcessor() )
 
 janapy.AddPlugin('jtest')
 janapy.Run()
+
+print('PYTHON DONE.')
 
