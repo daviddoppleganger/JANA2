a6a40fe0dbbadd145e22621c8781c76c9bccb721
David Lawrence <davidl@jlab.org>
2020-08-07 00:24:29 -0400

    Adding old DCluster.py and DHit.py files.

---
+++ src/python/examples/ex02_JEventProcessor/DCluster.py
@@ -0,0 +1,6 @@
+
+class DCluster():
+	def __init__(self):
+		print('DHit __init__ called.')
+		self.c = 1
+		self.d = 1.23
---
+++ src/python/examples/ex02_JEventProcessor/DHit.py
@@ -0,0 +1,6 @@
+
+class DHit():
+	def __init__(self):
+		print('DHit __init__ called.')
+		self.a = 1
+		self.b = 1.23
